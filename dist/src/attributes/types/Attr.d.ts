bdec3667e68f15493d46f878db0725ec999ee707
Gwenolé STEPHANT <gwenole@edumoov.com>
2020-12-06 20:10:07 +0100

    Tweak: add dist folder

---
+++ dist/src/attributes/types/Attr.d.ts
@@ -0,0 +1,19 @@
+import Record from '../../data/Record';
+import Model from '../../model/Model';
+import Mutator from '../contracts/Mutator';
+import Type from './Type';
+export default class Attr extends Type {
+    /**
+     * Hardcoded type for introspection
+     */
+    readonly className: string;
+    /**
+     * Create a new attr instance.
+     */
+    constructor(model: typeof Model, value: any, mutator?: Mutator<any>);
+    /**
+     * Make value to be set to model property. This method is used when
+     * instantiating a model or creating a plain object from a model.
+     */
+    make(value: any, _parent: Record, key: string): any;
+}
